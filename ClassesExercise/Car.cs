c8db5024617b20728f1e1dea355996f4e603c726
Saba Arefaine <arefainesaba@gmail.com>
2021-04-12 12:09:28 -0700

    inital commit

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ClassesExercise
+{
+    public class Car
+    {
+        //default constructor 
+        public Car()
+        {
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        //Create a Make property of type string that is public
+        public string Make { get; set; } //get = read set = write
+
+        //Create a Model property of type string that is public
+        public string Model { get; set; }
+
+        //Create a Year property of type int that is public
+        public int Year { get; set; }
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new instance of the Car class
+            //Car instead of var because we've created own type
+            Car myCar = new Car(); //constructor-helps create new instance of car class
+
+            //Set all of the object’s properties with a value
+            //dot notation
+            myCar.Make = "Toyota";
+            myCar.Model = "Corolla";
+            myCar.Year = 2016;
+
+            //OR
+            //object initializer
+            var honda = new Car()
+            {
+                Make = "Honda",
+                Model = "Accord",
+                Year = 2015
+            };
+
+            //OR
+            //passing through constructor
+            var subaru = new Car("Subaru", "Outback", 2017);
+
+            //Print the values of each property to the Console
+
+            //list with type Car constructor
+            var carList = new List<Car>() { myCar, honda, subaru };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
+          
+
+
+        }
+    }
+}
